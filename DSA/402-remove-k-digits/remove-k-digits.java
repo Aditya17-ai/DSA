8b82d2d56f6f44c6967958e61d8118d01112ea87
Aditya17-ai <130999483+Aditya17-ai@users.noreply.github.com>
2025-08-07 15:29:44 +0530

    Time: 28 ms (24.30%) | Memory: 45.4 MB (65.30%) - LeetSync

---
+++ DSA/402-remove-k-digits/remove-k-digits.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<num.length(); i++){
+            while(!st.isEmpty() && k>0 && 
+            (st.peek() > num.charAt(i)) ){
+                st.pop();
+                k--;
+            }
+            st.push(num.charAt(i));
+        }
+        //edge cases
+        while(k>0){
+            st.pop();
+            k--;
+        }
+
+        if(st.isEmpty()){
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        sb.reverse();
+        while (sb.length() > 0 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
+
+        
+    }
+}
